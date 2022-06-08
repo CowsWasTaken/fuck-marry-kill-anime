e36a4dd40793cc297ddf4006101c7cab980809e2
Felix J <felix.hannes.fj@gmail.com>
2022-06-08 15:56:13 +0200

    rate limit should work now

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const app = express();
 const limiter =  rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 })
-
-app.use(express.static(__dirname + "/dist/fuck-marry-kill-anime"), limiter);
+app.use(limiter)
+app.use(express.static(__dirname + "/dist/fuck-marry-kill-anime"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/fuck-marry-kill-anime/index.html"));
 });
